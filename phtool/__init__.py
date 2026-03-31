8a0d76984e00a5716e5e60373e88fa34690259f9
Dr Jie Zheng <jiezheng@nao.cas.cn>
2026-03-31 18:08:40 +0800

    append methods in init

--- phtool/__init__.py
+++ phtool/__init__.py
@@ -2,11 +2,20 @@
 
 
 """
-    v0 20250607, Dr/Prof Jie Zheng & Dr/Prof Linqiao Jiang
+    v0 20250607, Dr/AssoProf Jie Zheng & Dr/Prof Linqiao Jiang
     publish v0.1 20250717
+    v0.2 0.26.401
     Photometry Tools
 """
 
 
 from .biascomb import biascomb
 from .flatcomb import flatcomb
+from .imcorr import imcorr
+from .offset import offset
+from .find import find
+from .align import align
+from .phot import phot
+from .xyget import xyget
+from .pick import pick
+from .diffcali import diffcali
